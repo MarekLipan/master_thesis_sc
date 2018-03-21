d139856b2081632e96fe949011047e5b4e98387c
Marek Lipan <marek.lipan.ml@gmail.com>
2018-03-21 12:40:08 +0100

    Add the function to create balanced (filtered) panels of SPF

--- spf_data_preparation.py
+++ spf_data_preparation.py
@@ -8,7 +8,7 @@ This script is used to download, clean a manipulate the data before analysis.
 
 import pandas as pd
 import numpy as np
-import matplotlib.pylab as plt
+from sklearn import linear_model
 
 # load the data
 spf = pd.read_csv('/Users/Marek/Dropbox/Master_Thesis/Data/SPF/data.csv',
@@ -155,3 +155,136 @@ spf_raw_UNEM_2Y = spf_raw_UNEM_2Y[spf_raw_UNEM_2Y.index != "2001-12-01"]
 #######################
 # balanced panel data #
 #######################
+
+
+# function to create and fill a DataFrame with balanced panel data
+def create_balanced_df(df):
+    """
+    This function balances the panels of professional forecasters.
+
+    For balancing, is used the method proposed by Genre et. al. (2013).
+    Firstly, only the forecasters with no more than 4 consecutive observations
+    are kept. Secondly, the missing observations are imputed using the linear
+    filter. The beta is estimated from the following equation:
+
+        Dev_{t} = Beta * Dev_{t-1} + Epsilon_{t},
+
+    where Dev is deviation from the mean forecast of all forecasters. Then,
+    the estimated beta is used to impute forward the missing observations and
+    for simplification also missing observations from the beggining of the
+    sample (in case there are any).
+
+
+    Parameters
+    ----------
+    df : DataFrame
+        DataFrame containing the raw dataset
+
+    Returns
+    -------
+    DataFrame
+        DataFrame containing the balanced panels
+
+    """
+    # initialize vector determining which collumns to keep
+    keep = np.full(df.shape[1], True)
+
+    # only professionals with no more than 4 consecutive NaNs are kept
+    for j in range(df.shape[1]):
+        # amount of consecutive NaNs of a given professional
+        current_cons_nan = 0
+        max_cons_nan = 0
+
+        for i in range(df.shape[0]):
+
+            if np.isnan(df.iloc[i, j]):
+                current_cons_nan += 1
+
+                # in case of the last row, update immediately
+                if (i == df.shape[0]-1) & (current_cons_nan > max_cons_nan):
+                    max_cons_nan = current_cons_nan
+            else:
+                if current_cons_nan > max_cons_nan:
+                    max_cons_nan = current_cons_nan
+
+                current_cons_nan = 0
+
+        if max_cons_nan > 4:
+            keep[j] = False
+
+    # keep the frequently responding professionals
+    df = pd.DataFrame(df.loc[:, keep])
+
+    # for each professional (exclude ECB), calculate the deviation from the
+    # mean forecast and regress it on its lagged value, then use the beta to
+    # imput the missing values
+    mean_forecast = np.mean(df.iloc[:, 1:], axis=1)
+
+    for j in range(1, df.shape[1]):
+
+        dev_from_mean = df.iloc[:, j] - mean_forecast
+        lag_dev_from_mean = dev_from_mean.shift(periods=1)
+
+        # drop the NaNs before running the regression
+        reg_mat = pd.concat([lag_dev_from_mean, dev_from_mean],
+                            axis=1).dropna()
+
+        # create linear regression object
+        lin_reg = linear_model.LinearRegression(fit_intercept=False)
+
+        # fit the model
+        lin_reg.fit(reg_mat.iloc[:, 0].values.reshape(-1, 1),
+                    reg_mat.iloc[:, 1].values.reshape(-1, 1))
+
+        # store the estimated beta
+        beta = lin_reg.coef_
+
+        # find the postition of first non-nan observation
+        first_obs_pos = np.nan
+        k = 0
+        while (np.isnan(first_obs_pos) & (k <= df.shape[0])):
+            if (~np.isnan(df.iloc[k, j])):
+                first_obs_pos = k
+            k += 1
+
+        # forward imputation of missing observations
+        for i in range(first_obs_pos+1, df.shape[0]):
+            if (np.isnan(df.iloc[i, j])):
+                # imputed value = mean forecast + beta * past deviation
+                df.iloc[i, j] = float(
+                        mean_forecast[i] + beta * dev_from_mean[i-1])
+                # update the deviation from the mean vector
+                dev_from_mean[i] = float(df.iloc[i, j] - mean_forecast[i])
+
+        # backward imputation of missing observations
+        if first_obs_pos != 0:
+            for i in range(first_obs_pos-1, -1, -1):
+                # mean forecast + beta fraction of the first measured
+                # deviation (simplification)
+                df.iloc[i, j] = float(
+                        mean_forecast[i] + beta*dev_from_mean[first_obs_pos])
+
+    return pd.DataFrame(df)
+
+
+# real GDP 1Y horizon
+spf_bal_RGDP_1Y = create_balanced_df(spf_raw_RGDP_1Y)
+
+# real GDP 2Y horizon
+spf_bal_RGDP_2Y = create_balanced_df(spf_raw_RGDP_2Y)
+
+# HICP (inflation) 1Y horizon
+spf_bal_HICP_1Y = create_balanced_df(spf_raw_HICP_1Y)
+
+# HICP (inflation) 2Y horizon
+spf_bal_HICP_2Y = create_balanced_df(spf_raw_HICP_2Y)
+
+# unemployment 1Y horizon, correction for delayed survey
+spf_bal_UNEM_1Y = create_balanced_df(spf_raw_UNEM_1Y)
+
+# unemployment 2Y horizon, correction for delayed survey
+spf_bal_UNEM_2Y = create_balanced_df(spf_raw_UNEM_2Y)
+
+###############
+# END OF FILE #
+###############
